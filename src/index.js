82b16740f1ef1afff76f3ceec52c659fce2aca00
shinkwangwon <gwgogo@naver.com>
2018-11-27 23:12:49 +0900

    react_study 첫 테스트 커밋

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+// ReactDom.render 은 특정 ID를 가진 DOM에 <App> 컴포넌트를 그리겠다는 말
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
